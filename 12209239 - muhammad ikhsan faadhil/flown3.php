a9f6bfa469fa4b221d58ccd50984c10626dd8f46
ikhsan <muhammadikhsanfaadhil@smkwikrama.sch.id>
2023-08-25 11:03:49 +0700

    ikhsan

---
+++ 12209239 - muhammad ikhsan faadhil/flown3.php
@@ -0,0 +1,11 @@
+<?php
+$J = 2;
+$M = 120;
+$D = 100;
+
+$td = $J * 3600 + $M * 60 + $D;
+echo "2 Jam * 3600 + 120 Menit * 60 + 100 Detik )";
+echo "<br>";
+echo "<br>";
+echo "Total Detik = $td";
+?>
