afba5f5c008c4f55a12a90d8241f26ffbd34d824
asari <muhamadasari@smkwikrama.sch.id>
2023-08-25 13:08:05 +0700

    Pengumpulan tugas pengreplikaan website, dan soal 11-12 lkpd

---
+++ 12209150 - Muhammad As'ari/tugasUI/UILkpd12/beken.php
@@ -0,0 +1,84 @@
+<?php
+$jam1 = 2;
+$men1 = 13;
+$det1 = 23;
+$jam2 = 2;
+$men2 = 13;
+$det2 = 59;
+$jam3 = 1;
+$men3 = 59;
+$det3 = 59;
+$jam4 = 23;
+$men4 = 59;
+$det4 = 59;
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menambah Detik ke jam</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="form-container">
+        <h4>Jam Sebelum Penambahan</h4>
+        <h5><?= sprintf("%02d", $jam1) .":". $men1 . ":" . $det1; ?></h5>
+        <h5><?= sprintf("%02d", $jam2).":".$men2.":".$det2; ?></h5>
+        <h5><?= sprintf("%02d", $jam3).":".$men3.":".$det3; ?></h5>
+        <h5><?= "$jam4:$men4:$det4"; ?></h5>
+        <br>
+        <form action="" method="post">
+            <h4>Tambahan Detik</h4>
+            <input type="number" name="det">
+            <input type="submit" name="submit">
+        </form>
+<?php
+if (isset($_POST["det"])) {
+    $d = $_POST["det"];
+
+    $det1 += $d;
+    $det2 += $d;
+    $det3 += $d;
+    $det4 += $d;
+
+    $men1 += intdiv($det1, 60);
+    $men2 += intdiv($det2, 60);
+    $men3 += intdiv($det3, 60);
+    $men4 += intdiv($det4, 60);
+
+    $det1 %= 60;
+    $det2 %= 60;
+    $det3 %= 60;
+    $det4 %= 60;
+
+    $jam1 += intdiv($men1, 60);
+    $jam2 += intdiv($men2, 60);
+    $jam3 += intdiv($men3, 60);
+    $jam4 += intdiv($men4, 60);
+
+    $men1 %= 60;
+    $men2 %= 60;
+    $men3 %= 60;
+    $men4 %= 60;
+
+    $jam1 %= 24;
+    $jam2 %= 24;
+    $jam3 %= 24;
+    $jam4 %= 24;
+?>
+    <div class="sudh">
+    <br>
+    <h4>Jam sesudah ditambah <?= $d?> detik</h4>
+    <h5><?= sprintf("%02d", $jam1) . ":" . sprintf("%02d", $men1) . ":" . sprintf("%02d", $det1) ?></h5>
+    <h5><?= sprintf("%02d", $jam2) . ":" . sprintf("%02d", $men2) . ":" . sprintf("%02d", $det2) ?></h5>
+    <h5><?= sprintf("%02d", $jam3) . ":" . sprintf("%02d", $men3) . ":" . sprintf("%02d", $det3) ?></h5>
+    <h5><?= sprintf("%02d", $jam4) . ":" . sprintf("%02d", $men4) . ":" . sprintf("%02d", $det4) ?></h5>
+    </div>
+<?php } ?>
+</div>
+</body>
+
+</html>
