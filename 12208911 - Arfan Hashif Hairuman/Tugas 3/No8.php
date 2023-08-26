2d7591327b2426185ff8f1a5ec2b4a21fab1bcc7
arfnnhh <arfanhashifhairuman@smkwikrama.sch.id>
2023-08-26 20:40:36 +0700

    Tugas no 4, 7, 8, 9, 10, 19

---
+++ 12208911 - Arfan Hashif Hairuman/Tugas 3/No8.php
@@ -0,0 +1,12 @@
+<?php 
+
+echo "Bilangan bulat: ";
+$bb = readline();
+
+$s = $bb % 10;
+$p = $bb % 100;
+$r = $bb % 1000;
+
+echo "Ratusan: " . ($r - $p) / 100 . ", Puluhan: " . ($p - $s) / 10 . ", Satuan: " . $s;
+
+?>
---
+++ 12208911 - Arfan Hashif Hairuman/Tugas 3/No9.php
@@ -0,0 +1,14 @@
+<?php 
+echo "Input suhu (fahrenheit): ";
+$shf = readline();
+
+$shc = $shf / 33.8;
+
+if ($shc > 300) {
+    echo "Panas";
+} else if ($shc > 250) {
+    echo "Dingin";
+} else { 
+    echo "Normal";
+}
+?>
