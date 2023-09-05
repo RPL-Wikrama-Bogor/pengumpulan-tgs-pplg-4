e77815c3e4a8ffba4b7677fc2c7254a60977194c
asari <muhamadasari@smkwikrama.sch.id>
2023-09-05 09:08:37 +0700

    Added tugas kasir

--- 12209150 - Muhammad As'ari/tugasUI/UI11/index.php
+++ 12209150 - Muhammad As'ari/tugasUI/UI11/index.php
@@ -8,7 +8,7 @@
     <div class="form-container">
     <form action="" method="post">
         <label>Nomor karyawan</label><br>
-        <input type="number" name="no" placeholder="eg. 120201200704" class="top-input"><br>
+        <input type="number" name="no" placeholder="eg. 10107200701" class="top-input"><br>
         <input type="submit" name="submit">
     </form>
     
@@ -22,10 +22,12 @@
         } else {
             $ng = intval(substr($no, 0, 1));
             $tgl = intval(substr($no, 1, 2));
-            $bln = intval(substr($no, 4, 2));
-            $thn = intval(substr($no, 6, 4));
-            $nu = intval(substr($no, 10, 2));
+            $bln = intval(substr($no, 3, 2));
+            $thn = intval(substr($no, 5, 4));
+            $nu = intval(substr($no, 9, 2));
             ?>
+<!--            101207200702-->
+
             <h4>Nomor golongan: <?= $ng; ?></h4>
             <h4>Nomor urut: <?= $nu; ?></h4>
             <h4>Tanggal lahir: <?= $tgl . " " . $sbln[$bln - 1] . " " . $thn; ?></h4>
