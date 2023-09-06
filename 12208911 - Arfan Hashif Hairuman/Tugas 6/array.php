c89cc95d02352bfbd2b0bdb892644b516f47b3fc
arfnnhh <arfanhashifhairuman@smkwikrama.sch.id>
2023-09-06 09:04:55 +0700

    tugas array 06/09/2023

--- 12208911 - Arfan Hashif Hairuman/Tugas 6/array.php
+++ 12208911 - Arfan Hashif Hairuman/Tugas 6/array.php
@@ -8,35 +8,25 @@ echo "Sedangkan nilai yang terkecil adalah: " . min($n);
 echo "</br>";
 echo "Apabila diurutkan dari yang terkecil menjadi: ";
 sort($n);
-$arrlength = count($n);
-for($x = 0; $x < $arrlength; $x++) {
-    echo $n[$x];
-    echo " ";
-}
+echo implode(", ", $n);
 echo "</br>";
 echo "Apabila diurutkan dari yang terbesar menjadi: ";
 rsort($n);
-$arrlength = count($n);
-for($x = 0; $x < $arrlength; $x++) {
-    echo $n[$x];
-    echo " ";
-}
+echo implode(", ", $n);
+echo "</br>";
+$t = array_sum($n);
+$c = count($n);
+$m = $t / $c;
+$m = round($m,0);
+echo "Apabila dibulatkan rata-rata dari nilai saya menjadi: " . $m;
 echo "</br>";
 $cn = array_search(72, $n);
 if ($cn) {
     $n[$cn] = 75;
 }
 echo "Setelah melakukan perbaikan nilai 72 sekarang berubah menjadi 75, nilai saya sekarang: ";
-$arrlength = count($n);
-for($x = 0; $x < $arrlength; $x++) {
-    echo $n[$x];
-    echo " ";
-}
+echo implode(", ", $n);
 echo "</br>";
 echo "Sekarang apabila diurutkan dari yang terbesar menjadi: ";
 rsort($n);
-$arrlength = count($n);
-for($x = 0; $x < $arrlength; $x++) {
-    echo $n[$x];
-    echo " ";
-}
+echo implode(", ", $n);
