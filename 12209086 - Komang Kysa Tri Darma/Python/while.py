6e66b04b458e6f9c04603a603ed5678f056ec4ec
Komang <115518473+komangkysatridarma@users.noreply.github.com>
2023-09-07 09:36:01 +0700

    Delete 12209086 - Komang Kysa Tri Darma/Python directory

--- 12209086 - Komang Kysa Tri Darma/Python/while.py
+++
@@ -1,4 +0,0 @@
-j = 0
-while j<=10:
-  print(j)
-  j = j + 1
--- 12209086 - Komang Kysa Tri Darma/Python/whileif.py
+++
@@ -1,6 +0,0 @@
-lanjut = True
-while(lanjut):
- option = input("lanjut atau tidak? (y/n)")
- if(option == 'n'):
-  lanjut == False
-  print("Program Berhenti")
