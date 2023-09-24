28703cf2b9c034a79c55e4bc4b40a5e5b391e4e3
asari <muhamadasari@smkwikrama.sch.id>
2023-09-24 09:18:07 +0700

    Minor  changes, added tugasNilai.php

--- 12209150 - Muhammad As'ari/Bensin.php
+++ 12209150 - Muhammad As'ari/Bensin.php
@@ -1,5 +1,4 @@
-<?php
-
+<?php 
 class Shell
 {
     protected $hargaSSuper = 154200;
@@ -63,4 +62,32 @@ class Beli extends Shell
     }
 }
 
+$text = false;
+
+if (isset($_POST["submit"])) { 
+    $text = true;
+    $productName = $_POST["select"];
+    $jumlah = $_POST["jumlah"];
+    $model = new Beli($productName, $jumlah);
+}
 ?>
+
+<body>
+    <?php if($text == true) {?>    
+        <div class="text">
+            <p>Anda membeli bahan bakar minyak tipe <?= $model->getTipeBensin() ?> </p>
+            <p>Dengan harga : <?= number_format($model->getTotalHarga(), 1, ",", ".") ?></p>
+            <p>Total liter : <?= $model->getTotalLiter() ?></p>
+        </div>
+    <?php } ?>
+    <form action="" method="post">
+        <select name="select">
+            <option value="SS">Shell Super</option>
+            <option value="SVP">Shell V-Power</option>
+            <option value="SVPD">Shell V-Power Diesel</option>
+            <option value="SVPN">Shell V-Power Nitro</option> 
+        </select><br>
+        <input type="text" name="jumlah" placeholder="total liter"><br>
+        <input type="submit" name="submit">
+    </form>
+</body>
--- 12209150 - Muhammad As'ari/tugasRental.php
+++ 12209150 - Muhammad As'ari/tugasRental.php
@@ -1,107 +1,141 @@
 <?php
-class Motor {
-    protected $registeredMember = ["Siti", "Asep", "Angga", "Adit", "Manda"];
-    protected $motor = [
-        [
-            "Merk" => "Scoopy",
-            "Price" => 10000
-        ],
-        [
-            "Merk" => "Yamaha",
-            "Price" => 20000
-        ],
-        [
-            "Merk" => "Kawasaki",
-            "Price" => 30000
-        ],
-        [
-            "Merk" => "Honda",
-            "Price" => 40000
-        ],
-        [
-            "Merk" => "BMW",
-            "Price" => 50000
-        ],
-    ];
+$conn = new mysqli("localhost", "root", "", "rentalMotor");
+
+class Motor
+{
+    protected $registeredMember = ["Siti", "Asep", "Angga", "Adit", "Manda", ""];
 
     public function getMotorData()
     {
-        return $this->motor;
-    }
-}
+        global $conn;
+        $sql = "SELECT merk, price FROM main";
+        $result = $conn->query($sql);
+        $motorData = [];
 
-class Rental extends Motor {
-    protected $price = 0;
-    protected $discount = 10000;
-    protected $discountedPrice = 0;
-    protected $tax = 1.1;
-
-    public function __construct($user, $jenisMotor, $durasi)
-    {
-        $this->calculatePrice($user, $jenisMotor, $durasi);
-    }
-
-    protected function calculatePrice($user, $jenisMotor, $durasi)
-    {
-        // Check if the selected motor type exists in the array
-        $motorData = $this->getMotorData();
-        $motorTypeExists = false;
-        foreach ($motorData as $motorItem) {
-            if ($motorItem["Merk"] === $jenisMotor) {
-                $motorTypeExists = true;
-                $this->price = $motorItem["Price"];
-                break;
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                $motorData[] = $row;
             }
         }
 
-        if (!$motorTypeExists) {
-            echo "Motor type not found in the available options.";
-            return;
-        }
-
-        // Calculate the total price
-        $totalPrice = $this->price * $durasi;
-
-        // Apply discounts and tax
-        if (in_array($user, $this->registeredMember)) {
-            $totalPrice -= $this->discount;
-        }
-
-        $this->discountedPrice = $totalPrice * $this->tax;
+        return $motorData;
     }
 
-    public function getPrice()
+    public function updateMotorPrice($merk, $newPrice)
     {
-        return $this->discountedPrice;
+        global $conn;
+        $sql = "UPDATE main SET price = $newPrice WHERE merk = '$merk'";
+        $conn->query($sql);
     }
 }
+
+class Rent extends Motor
+{
+    public $originPrice = 0;
+    public $duration = 0;
+    public $product = "";
+    public $user = "";
+    public $discount = 1;
+    public $finalPrice = 0;
+    public $tax = 10000;
+    public $member = false;
+
+    public function __construct($user, $product, $duration)
+    {
+        $this->product = $product;
+        $this->user = $user;
+        $this->duration = $duration;
+
+        $this->isMember();
+        $this->setPrice();
+        $this->cetakOutput();
+    }
+
+    public function isMember()
+    {
+        if (in_array($this->user, $this->registeredMember)) {
+            $this->discount = 0.95;
+            $this->member = true;
+        }
+    }
+    public function setPrice()
+    {
+        $motorData = $this->getMotorData();
+        foreach ($motorData as $motorcycle) {
+            if ($motorcycle['merk'] == $this->product) {
+                $this->originPrice = $motorcycle['price'];
+                break;
+            }
+        }
+        $this->finalPrice = ($this->originPrice * $this->duration) * $this->discount + $this->tax;
+    }
+
+    public function cetakOutput()
+    { 
+        echo "$this->user Berstatus sebagai " . ($this->member ? "member terdaftar. Diskon sebesar $this->discount" : "bukan member terdaftar") . "<br>";
+        echo "Jenis motor yang dirental $this->product selama $this->duration" . " hari <br>";
+        echo "Harga rental per harinya : $this->originPrice" . "<br>";
+        echo "Besar yang harus dibayarkan adalah : $this->finalPrice" . "<br>";
+    }
+}
+
+$MotorInstance = new Motor();
 ?>
 
+<head>
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+        }
+  
+        .secondForm {
+            margin-top: 40px;
+        }
+    </style>
+</head>
 <form action="" method="post">
-    <h4>Nama :</h4>
-    <input type="text" name="nama">
-    <h4>Jenis motor </h4>
+    <h4>Nama :</h4> <input type="text" name="nama">
+    <h4>Jenis motor</h4>
     <select name="jenisMotor">
-        <?php $motorObj = new Motor; foreach ($motorObj->getMotorData() as $item) {?>
-        <option value="<?= $item["Merk"] ?>"><?= $item["Merk"] ?></option>
-        <?php } ?>
+        <?php
+        foreach ($MotorInstance->getMotorData() as $motorcycle) {
+            echo "<option>{$motorcycle['merk']}</option>";
+        }
+        ?>
     </select>
-    <h4>Durasi : </h4>
-    <input type="text" name="durasi">
-    <input type="submit" name="submit">
+    <h4>Durasi : </h4> <input type="text" name="durasi">
+    <input type="submit" name="submit" value="kirimData">
 </form>
 
-<?php 
-if (isset($_POST["submit"])) {
+<?php
+if (isset($_POST["submit"]) && $_POST["submit"] == "kirimData") {
     $nama = $_POST["nama"];
     $jenisMotor = $_POST["jenisMotor"];
     $durasi = $_POST["durasi"];
-    
-    // Create a Rental object
-    $rental = new Rental($nama, $jenisMotor, $durasi);
 
-    // Get the final price
-    $finalPrice = $rental->getPrice();
-    echo "Total Price: $" . number_format($finalPrice, 2);
+    $rent = new Rent($nama, $jenisMotor, $durasi);
+}
+?>
+
+<form action="" method="POST" class="secondForm">
+    <h4>Tipe</h4>
+    <select name="tipe">
+        <?php
+        foreach ($MotorInstance->getMotorData() as $motorcycle) {
+            echo "<option>{$motorcycle['merk']}</option>";
+        }
+        ?>
+    </select>
+    <h4>Nilai baru</h4>
+    <input type="number" name="nilaiBaru">
+    <input type="submit" name="submit" value="updateData">
+</form>
+
+<?php
+if (isset($_POST["submit"]) && $_POST["submit"] == "updateData") {
+    $tipe = $_POST["tipe"];
+    $nilaiBaru = $_POST["nilaiBaru"];
+    $MotorInstance->updateMotorPrice($tipe, $nilaiBaru);
 }
 ?>
