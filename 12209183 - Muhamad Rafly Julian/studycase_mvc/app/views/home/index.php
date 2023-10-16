1160460c93feb6a82892ff3987d12dcfc954a9ca
raflyjulian <mraflyjulian@gmail.com>
2023-10-16 14:55:30 +0700

    tugasmvc

---
+++ 12209183 - Muhamad Rafly Julian/studycase_mvc/app/views/home/index.php
@@ -0,0 +1,16 @@
+<div class="container">
+    <div class="jumbotron py-4 px-5"></div>
+        <h1 class="display-4">Selamat Datang!</h1>
+        <hr class="my-4">
+        <h3>Inventaris Alat Pembelajaran</h3>
+        <br>
+        <br>
+        <h4>Peraturan Peminjaman</h4>
+        <ol>
+            <li>Menemui Petugas invertaris ketika akan memunjam dan mengembalikan barang</li>
+            <li>Lama waktu peminjaman hanya 2 hari</li>
+            <li>Menyerahkan kartu identitas ketika melakukan peminjaman sebagai jaminan</li>
+        </ol>
+        
+    </div>
+</div>
