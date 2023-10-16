073a52781c2dac1bc49b30e407268d2f2f8e2847
Ramon hidayat <hidayatmramon@gmail.com>
2023-10-16 13:10:22 +0700

    tugas oop dan array

--- 12209364 - Ramon Hidayat/MVC-PDO/Inventaris/app/config/Config.php
+++ 12209364 - Ramon Hidayat/MVC-PDO/Inventaris/app/config/Config.php
@@ -1,6 +1,6 @@
 <?php
 
-define('BASE_URL', 'https://localhost/pengumpulan-tgs-pplg-4/12209364%20-%20Ramon%20Hidayat/MVC-PDO/inventaris/public');
+define('BASE_URL', 'http://localhost/pengumpulan-tgs-pplg-4/12209364%20-%20Ramon%20Hidayat/MVC-PDO/inventaris/public');
 
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
