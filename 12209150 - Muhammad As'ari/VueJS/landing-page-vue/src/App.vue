1bacd55c1ecf843390923ff4f33c971b1affeceb
asari <muhamadasari@smkwikrama.sch.id>
2023-11-02 08:39:40 +0700

    Upload tugas landing page with router applied, projek ke 4.

---
+++ 12209150 - Muhammad As'ari/VueJS/landing-page-vue/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <navbar></navbar>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script setup>
+import Navbar from './components/layouts/Navbar.vue';
+// import Header from './components/Beranda/Header.vue';
+// import Service from './components/Beranda/Service.vue';
+// import Portofolio from './components/Beranda/Portofolio.vue';
+// import Blog from './components/Beranda/Blog.vue';
+</script>
+
+<style>
+html, 
+body {
+  padding: 0;
+  margin: 0;
+  font-family: 'Poppins', sans-serif;
+}
+</style>
---
+++ 12209150 - Muhammad As'ari/VueJS/landing-page-vue/src/components/Beranda/Header.vue
@@ -0,0 +1,75 @@
+<template>
+    <div class="header-container">
+        <div class="header-text">
+            <div class="header-title">
+                <h1>Experts are here to help with your bussiness problem</h1>
+            </div>
+            <div class="header-description">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, nobis? Earum id maiores vero
+                    delectus? Quod ad dolor, necessitatibus deleniti molestias iusto</p>
+            </div>
+        </div>
+        <div class="header-image">
+            <img src="@/assets/img/section_1.jpg" alt="section1">
+        </div>
+    </div>
+</template>
+
+<style>
+.header-container {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 20px;
+    background-color: #f7f7f7;
+}
+
+.header-text {
+    color: #333333be;
+    flex: 1;
+    margin-right: 20px;
+}
+
+.title-text {
+    font-size: 24px;
+    font-weight: bold;
+    color: #333;
+}
+
+.description-text {
+    font-size: 16px;
+    color: #666;
+}
+
+.header-image {
+    flex: 1;
+}
+
+.header-img {
+    width: 100%;
+    max-width: 100%;
+    height: 26px;
+    border-radius: 10px;
+}
+
+@media screen and (max-width: 768px) {
+    .header-container {
+        flex-direction: column;
+        text-align: center;
+    }
+    
+    .header-text {
+        margin-right: 0;
+        margin-bottom: 20px;
+    }
+
+    .title-text {
+        font-size: 20px;
+    }
+
+    .description-text {
+        font-size: 14px;
+    }
+}
+</style>
+
