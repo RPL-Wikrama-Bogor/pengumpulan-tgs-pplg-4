4a45dacc41f5bfb523344a23d0ba99a3ce3b76da
dheanic <dheanickyta71@gmail.com>
2023-11-24 10:06:25 +0700

    tgas

---
+++ 12208966 - Dhea Nickyta Az Zahra/front_end/wikrama/component-vue/src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="row-service">
+    <Card v-for="item in data" :title="item.title" :description="item.description" >
+      <p>{{ item.date }}</p>
+    </Card>
+    <!-- <card-service title="titel 2" description="kedua"></card-service> -->
+  </div>
+  <button class="btn-service">Detail</button>
+  <MyBtn id="btn1" name="btn"></MyBtn> <!---membuat button clone-->
+  <MyBtn id="btn2" name="btn2"></MyBtn>
+</template>
+
+<script>
+import Card from '@/components/my-components/Card.vue';
+export default {
+  components: {
+    Card,
+    'card-service': Card
+  },
+  data() {
+    return {
+      data: [
+        {
+          title: 'title 1', //untuk looping
+          description: 'description 2',
+          date: '31/10/2023'
+        },
+        {
+          title: 'title 1',
+          description: 'description 2',
+          date: '31/10/2023'
+        }
+      ]
+    }
+  }
+}
+
+</script>
+<style>
+.row-service {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 10px;
+}
+.btn-service-global {
+        margin-top: 10px;
+        font-size: 16px;
+        font-weight: 400;
+        color: white;
+        background-color: #8EACCD;
+        padding: 10px 20px;
+        border: none;
+        border-radius: 5px;
+    }
+</style>
---
+++ 12208966 - Dhea Nickyta Az Zahra/front_end/wikrama/compro-wikrama-pertemuan/src/pages/Portofolio.vue
@@ -0,0 +1,81 @@
+<template>
+    <div class="container">
+        <div class="protofolio">
+            <h3>Portofolio kami</h3>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+            <div class="category">
+                <span>Web</span>
+                <span>Desain</span>
+                <span>Mobile</span>
+            </div>
+            <div class="row-portofolio">
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<style>
+.category {
+    margin: 10px 0 35px 0;
+    display: flex;
+    flex-wrap: wrap;
+}
+
+.category span {
+    background-color: white;
+    padding: 10px 15px;
+    font-weight: 500;
+    border-radius: 20px;
+    margin: 5px;
+    cursor: pointer;
+}
+
+.row-portofolio {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-gap: 10px;
+}
+
+.portofolio {
+    margin-top: 10px;
+}
+
+.portofolio h3 {
+    margin-top: 10px;
+    font-weight: 900;
+    font-size: 30px;
+    line-height: 35px;
+    margin-bottom: 0;
+    color: blue;
+}
+
+.portofolio p {
+    font-weight: 900;
+    font-size: 14px;
+    line-height: 20px;
+    color: #4f556a;
+    max-width: 680px;
+    margin: auto;
+    margin-top: 14px;
+    margin-bottom: 25px;
+    text-align: center;
+}
+
+.portofolio a span {
+    color: gray;
+}
+
+@media screen and (max-width: 600px) {
+    
+    .row-portofolio {
+        display: grid;
+        grid-template-columns: repeat(1, 1fr);
+        grid-gap: 10px;
+    }
+
+    .portofolio h3 {
+        font-size: 20px;
+    }
+}
+</style>
